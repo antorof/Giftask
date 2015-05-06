3e8ab60075062328d1b53183305c49c693f2eca0
DavidGSola <hispalos@gmail.com>
2015-05-06 18:14:05 +0200

    Añadido efecto reshulon a los botones de detalle de un regalo

--- app/src/main/java/es/trigit/gitftask/Pantallas/GiftDetalleFragment.java
+++ app/src/main/java/es/trigit/gitftask/Pantallas/GiftDetalleFragment.java
@@ -1,6 +1,7 @@
 package es.trigit.gitftask.Pantallas;
 
 import android.app.Fragment;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,15 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import at.markushi.ui.RevealColorView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import es.trigit.gitftask.Objetos.Regalo;
 import es.trigit.gitftask.R;
 import es.trigit.gitftask.Utiles.Globales;
-
 /**
  * Created by DavidGSola on 22/03/2015.
  */
@@ -44,8 +46,8 @@ public class GiftDetalleFragment extends Fragment {
     @InjectView(R.id.gift_detalle_boton_repost)
     ImageButton mBotonRepost;
 
-    @InjectView(R.id.gift_detalle_boton_me_gusta)
-    ImageButton mBotonMeGusta;
+    @InjectView(R.id.gift_detalle_layout_me_gusta)
+    RelativeLayout mBotonMeGusta;
 
     @InjectView(R.id.gift_detalle_boton_lo_tengo)
     ImageButton mBotonLoTengo;
@@ -58,6 +60,18 @@ public class GiftDetalleFragment extends Fragment {
 
     @InjectView(R.id.gift_detalle_usuario_nombre)
     TextView mUsuarioNombre;
+
+    @InjectView(R.id.reveal_1)
+    RevealColorView revealColorView_1;
+
+    @InjectView(R.id.reveal_2)
+    RevealColorView revealColorView_2;
+
+    @InjectView(R.id.reveal_3)
+    RevealColorView revealColorView_3;
+
+    @InjectView(R.id.reveal_4)
+    RevealColorView revealColorView_4;
 
 
     public static GiftDetalleFragment newInstance() {
@@ -97,25 +111,81 @@ public class GiftDetalleFragment extends Fragment {
         r.setUsuarioPropietario(Globales.getUsuarioLogueado());
 
         Globales.addRegalo(r);
+        animarBoton(button, revealColorView_1);
 
         Log.v(TAG, "Repost");
     }
 
-    @OnClick(R.id.gift_detalle_boton_me_gusta)
-    public void pulsarMeGusta(ImageButton button){
+    @OnClick(R.id.gift_detalle_layout_me_gusta)
+    public void pulsarMeGusta(RelativeLayout button){
         mRegalo.addMeGusta(Globales.getUsuarioLogueado().getId());
         Globales.addRegalo(mRegalo);
         mNumeroMeGusta.setText(mRegalo.getNumLikes()+"");
+
+        animarBoton(button, revealColorView_2);
         Log.v(TAG, "Me gusta");
     }
 
     @OnClick(R.id.gift_detalle_boton_lo_tengo)
     public void pulsarLoTengo(ImageButton button){
+        animarBoton(button, revealColorView_3);
         Log.v(TAG, "Lo tengo");
     }
 
     @OnClick(R.id.gift_detalle_boton_compartir)
-    public void pulsarCompatrir(ImageButton button){
+    public void pulsarCompatrir(ImageButton button)
+    {
+        animarBoton(button, revealColorView_4);
         Log.v(TAG, "Compartir");
+    }
+
+    private void animarBoton(final View view, final RevealColorView revealColor)
+    {
+        reavealColor(view, revealColor);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        hideColor(view, revealColor);
+                    }
+                });
+            }
+        };
+        thread.start();
+    }
+
+    private void reavealColor(View view, RevealColorView revealColor)
+    {
+        final Point p = getLocationInView(revealColor, view);
+        final int color = getResources().getColor(R.color.LIME_500);
+        revealColor.reveal( p.x, p.y, color, view.getHeight() / 2, 340, null);
+    }
+
+    private void hideColor(View view, RevealColorView revealColor)
+    {
+        final Point p = getLocationInView(revealColor, view);
+        final int color = getResources().getColor(R.color.transparente);
+        revealColor.hide(p.x, p.y, color, 0, 300, null);
+    }
+
+    private Point getLocationInView(View src, View target) {
+        final int[] l0 = new int[2];
+        src.getLocationOnScreen(l0);
+
+        final int[] l1 = new int[2];
+        target.getLocationOnScreen(l1);
+
+        l1[0] = l1[0] - l0[0] + target.getWidth() / 2;
+        l1[1] = l1[1] - l0[1] + target.getHeight() / 2;
+
+        return new Point(l1[0], l1[1]);
     }
 }
